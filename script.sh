5c69003d0256236f3035877b69a0313d6d3125f4
Denis <denis_kulikov_a_2003@mail.ru>
2024-12-02 23:37:02 +0700

    Update script

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-bison -Wcounterexamples -d -o parser.tab.c cpp-parser.y
+bison -d -o parser.tab.c cpp-parser.y
 flex -o lexer.c lexer.l
 g++ parser.tab.c lexer.c parser.cpp -o parser -lfl
